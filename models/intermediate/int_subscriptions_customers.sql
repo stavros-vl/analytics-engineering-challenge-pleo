8fa6a1e35b3a37731029afc078d85b2522f7d490
Stavros Vlachos <svlachos25@gmail.com>
2023-06-14 09:07:00 +0000

    Refactor mrr_base

--- models/intermediate/int_subscriptions_customers.sql
+++ models/intermediate/int_subscriptions_customers.sql
@@ -2,15 +2,16 @@ WITH base AS (
     SELECT 
         invoices.invoice_id,
         invoices.customer_id,
-        subscriptions.period_start_day,
-        subscriptions.period_end_day,
-        subscriptions.converted_amount
+        mrr_base.period_start_day,
+        mrr_base.period_end_day,
+        mrr_base.converted_amount,
+        mrr_base.mrr
     FROM 
-        {{ ref("int_subscriptions") }} subscriptions
+        {{ ref("mrr_base") }} mrr_base
     INNER JOIN 
         {{ ref("stg_invoice") }} invoices
     ON 
-        invoices.invoice_id = subscriptions.id
+        invoices.invoice_id = mrr_base.id
 
 )
 SELECT 
